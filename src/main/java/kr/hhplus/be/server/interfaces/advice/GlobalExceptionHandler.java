37d97b233a873e0cdd405572ac603ffa04d54d38
sabsiru <zero013@naver.com>
2025-04-10 00:21:24 +0900

    feat: repository 구현체 작성 및 예외 핸들러 작성
     - 컨트롤러 단위 테스트를 진행하기 위한 사전 작업

---
+++ src/main/java/kr/hhplus/be/server/interfaces/advice/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package kr.hhplus.be.server.interfaces.advice;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    // 필요에 따라 다른 예외 처리 메서드 추가
+}
