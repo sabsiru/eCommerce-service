cdc7b840f56118ebd27edd7b1653e346ede3c409
sabsiru <zero013@naver.com>
2025-04-24 17:05:41 +0900

    feat : 인기상품 조회를 위한 조회 테이블 배치 스케줄러 구현

--- src/main/java/kr/hhplus/be/server/domain/product/ProductReadService.java
+++ src/main/java/kr/hhplus/be/server/domain/product/ProductReadService.java
@@ -1,4 +1,4 @@
-package kr.hhplus.be.server.application.product;
+package kr.hhplus.be.server.domain.product;
 
 import kr.hhplus.be.server.infrastructure.product.ProductQueryRepository;
 import kr.hhplus.be.server.infrastructure.product.ProductSummaryRow;
